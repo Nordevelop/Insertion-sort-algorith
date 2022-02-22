724c73a11def737bd2afe6b1de2c54cb926cf382
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-21 23:54:56 -0300

    Update ArrayInserccion.java

--- ArrayInserccion.java
+++ ArrayInserccion.java
@@ -4,46 +4,45 @@ import java.util.*;
 
 
 public class ArrayInserccion {
+	          /*array encapsulado con acceso denegado en la clase Main()
+		  * Author Nordevelop
+		  */
 	
-		private long[] a;     /*array encapsulado con acceso denegado en la clase Main()
-				       * Author Nordevelop
-				       */
+		private long[] a;   
 		private int nElementos;  
 
-		public  ArrayInserccion(int max)  
-		{
+		public  ArrayInserccion(int max){  //clase constructor
 		   a = new long[max]; 
-		   nElementos = 0; // array vacio
+		   nElementos = 0;
 		}
+	        /*
+		* Metodos
+		*/
 
-		public void agregar(long valor)  // metodo para agregar elementos dentro  del array
-		{
-		a[nElementos] = valor;  
-		nElementos++;         
-		}
+		public void agregar(long valor){ // metodo para agregar elementos dentro  del array
+		   a[nElementos] = valor;  
+		   nElementos++;         
+		}//fin metodo agregar()
 
-		public void mostrar()  // mostrar contenido del Array en pantalla
-		{
-		for(int j=0; j<nElementos; j++)
-		 // for each ,
-		   System.out.print(a[j] + " ");     
-		   System.out.println(" ");
-		}
+		public void mostrar(){  // mostrar contenido del Array en pantalla
 		
-		public void inserccionSort()
-		{
-		int entrada, salida;
-		for(salida=1; salida<nElementos; salida++)   
-		{
-		   long aux = a[salida];     
-		   entrada = salida;        
-		   while(entrada>0 && a[entrada-1] >= aux) // 
-		   	{
-		      a[entrada] = a[entrada-1];    
-	     	  --entrada;            
-		}
+		  for(int j=0; j<nElementos; j++){
+		      System.out.print(a[j] + " ");     
+		      System.out.println(" ");
+		  }	
+		}//fin metodo mostrar()
+		
+		public void inserccionSort(){
+		  int entrada, salida;
+		  for(salida=1; salida<nElementos; salida++){
+		     long aux = a[salida];     
+		     entrada = salida;        
+		     while(entrada>0 && a[entrada-1] >= aux){
+			  a[entrada] = a[entrada-1];    
+	     	          --entrada;            
+		     }//end while
 		a[entrada] = aux;       
-		} // fin for
+		} // fin for salida
 		} // fin metodo inserccionSort()
 		
 		} // Fin clase ArrayInserccion
